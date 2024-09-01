c471a6ad764464fbac6f8992778f3dfbefd54671
Juan David Rengifo Castro <jdrengifoc@eafit.edu.co>
2024-09-01 01:10:37 -0500

    Complete LinkedList class.

--- src/lab.py
+++ src/lab.py
@@ -1,3 +1,14 @@
 from linkedlist import LinkedList
 
 ll = LinkedList()
+
+ll.insert_at_head(1)
+ll.insert_at_head(2)
+ll.insert_at_head(3)
+ll.remove_at_head()
+ll.insert_at_tail(4)
+
+ll.insert_at("a", 1)
+print(ll)
+ll.delete_at(3)
+print(ll)
